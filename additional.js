3232a0a9ec9707f061632ba74511c3411e5809cf
Giga-Gachava <gghac20@freeuni.edu.ge>
2023-12-25 06:46:15 +0400

    add information of mini-container

---
+++ additional.js
@@ -0,0 +1,50 @@
+// {
+//   ("data");
+//   [
+//     {
+//       id: 1,
+//       title: "Blog title",
+//       description: "Blog description",
+//       image: "https://via.placeholder.com/150",
+//       publish_date: "2023-11-19 00:00:00",
+//       categories: [
+//         {
+//           id: 1,
+//           name: "Category name",
+//           text_color: "#ffffff",
+//           background_color: "#000000",
+//         },
+//       ],
+//       author: "გელა გელაშვილი",
+//     },
+//   ];
+// }
+
+// {
+//   ("message");
+//   "The categories field must be an array.", "errors";
+//   {
+//     ("categories");
+//     ["The categories field must be an array."];
+//   }
+// }
+
+// {
+//   ("id");
+//   1, "title";
+//   "Blog title", "description";
+//   "Blog description", "image";
+//   "https://via.placeholder.com/150", "publish_date";
+//   "2023-11-19 00:00:00", "categories";
+//   [
+//     {
+//       id: 1,
+//       title: "Category name",
+//       text_color: "#ffffff",
+//       background_color: "#000000",
+//     },
+//   ],
+//     "author";
+//   "გელა გელაშვილი", "email";
+//   ("gigagiorgadze@redberry.ge");
+// }
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const getUsers = async () => {
+  try {
+    const users = await fetch(`../additional.js`).then((_) => _.json());
+    users.forEach((user) => {
+      console.log(user.id);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+getUsers();
+
+// fetch(`../additional.js`)
+//   .then((Response) => Response.json())
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
